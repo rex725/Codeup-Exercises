c50d655f7d0e68ac9f2934cea030697c092d57e7
David R. Jex <david.jex@me.com>
2017-07-18 16:16:29 -0500

    Fixed code to have new lines after every if statement.

--- if_and_or.php
+++ if_and_or.php
@@ -14,27 +14,27 @@ if ($x < $y && $y < $z) {
 // If 0 is less than $x OR $x is less than 10
 // then echo the result as a sentence "0 is less than {$x} OR {$x} is less than 10".
 if (0 < $x || $x < 10) {
-	echo "0 is less than {$x} OR {$x} is less than 10";
+	echo "0 is less than {$x} OR {$x} is less than 10\n";
 }
 // TODO:
 // repeat the if statement for $y and $z.
 if (0 < $y || $x < 10) {
-	echo "0 is less than {$x} OR {$x} is less than 10";
+	echo "0 is less than {$x} OR {$x} is less than 10\n";
 }
 if (0 <$z || $z < 10) {
-	echo "0 is less than {$x} OR {$x} is less than 10";
+	echo "0 is less than {$x} OR {$x} is less than 10\n";
 }
 // TODO:
 // If 0 is less than $x AND $x is less than 10
 // then echo the result as a sentence "0 is less than {$x} AND {$x} is less than 10".
 if (0 < $x && $x < 10) {
-	echo "0 is less than {$x} AND {$x} is less than 10";
+	echo "0 is less than {$x} AND {$x} is less than 10\n";
 }
 // TODO:
 // repeat the if statement for $y and $z.
 if (0 < $y && $y < 10) {
-	echo "0 is less than {$x} AND {$x} is less than 10";
+	echo "0 is less than {$x} AND {$x} is less than 10\n";
 }
 if (0 < $z && $z < 10) {
-	echo "0 is less than {$x} AND {$x} is less than 10";
+	echo "0 is less than {$x} AND {$x} is less than 10\n";
 }
