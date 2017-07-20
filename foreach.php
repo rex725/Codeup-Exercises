93f5ed81c91d2608a17bdc658ef984f616ccc1a2
David R. Jex <david.jex@me.com>
2017-07-20 12:12:16 -0500

    Fizzbuzz app and foreach loop that iterates through list of things and returns the values of what they are.

---
+++ foreach.php
@@ -0,0 +1,17 @@
+<?php
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+foreach($things as $thing) {
+	if (is_int($thing)) {
+		fwrite(STDOUT, "{$thing} is an integer.\n");
+	} else if (is_float($thing)) {
+		fwrite(STDOUT, "{$thing} is a float.\n");
+	} else if (is_bool($thing)) {
+		fwrite(STDOUT, "{$thing} is a boolean.\n");
+	} else if (is_array($thing)) {
+		fwrite(STDOUT, "{$thing} is an array.\n");
+	} else if (is_null($thing)) {
+		fwrite(STDOUT, "{$thing} is a null value.\n");
+	} else if (is_string($thing)) {
+		fwrite(STDOUT, "{$thing} is a string.\n");
+	}
+}
