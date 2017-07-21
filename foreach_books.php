9b113abfb0d78a02b96db12174d3a952102633f7
David R. Jex <david.jex@me.com>
2017-07-21 16:23:14 -0500

    Exercises.

--- foreach_books.php
+++ foreach_books.php
@@ -6,6 +6,11 @@ $books = array(
         'author' => 'J. R. R. Tolkien',
         'pages' => 310
     ),
+    'The Lord of the Rings' => array(
+    	'published' => 1968,
+    	'author' => 'J. R. R. Tolkien',
+    	'pages' => 1178
+    ),
     'Game of Thrones' => array(
         'published' => 1996,
         'author' => 'George R. R. Martin',
@@ -22,7 +27,6 @@ $books = array(
         'pages' => 544
     )
 );
-echo $books['The Hobbit']['published'];
 foreach($books as $key => $value) {
 	echo "$key".PHP_EOL.PHP_EOL;
 	foreach($value as $key => $value) {
@@ -37,5 +41,46 @@ foreach($books as $key => $value) {
 		foreach($value as $key => $value) {
 			echo " {$key}: {$value}".PHP_EOL;
 		}
+	}
+}
+
+fwrite(STDOUT, 'BOOKS WITH LESS THAN 300 PAGES'.PHP_EOL);
+foreach($books as $key => $value) {
+	if ($books[$key]['pages'] < 300) {
+		echo "$key".PHP_EOL.PHP_EOL;
+		foreach($value as $key => $value) {
+			echo " {$key}: {$value}".PHP_EOL;
+		}
+	}
+}
+
+fwrite(STDOUT, 'BOOKS AVERAGE PAGE LENGTH AND YEAR OF PUBLICATION'.PHP_EOL);
+$averageLength = 0;
+$averagePublishingDate = 0;
+foreach($books as $key => $value) {
+	$averageLength += $books[$key]['pages'];
+	$averagePublishingDate += $books[$key]['published'];
+}
+echo $averageLength/count($books).PHP_EOL;
+echo $averagePublishingDate/count($books).PHP_EOL;
+
+fwrite(STDOUT, 'NO DUPLICATE BOOKS'.PHP_EOL);
+
+$authors = [];
+foreach($books as $key => $value) {
+	array_push($authors, $books[$key]['author']);
+
+	for ($i = 0; $i < count($authors); $i++) {
+		if ($i >= 1) {
+			if ($books[$key]['author'] == $authors[$i - 1]) {
+				unset($books[$key]);
+			}
+		}		
+	}
+}
+foreach ($books as $key => $value) {
+	echo "$key".PHP_EOL.PHP_EOL;
+	foreach($value as $key => $value) {
+		echo " {$key}: {$value}".PHP_EOL;
 	}
 }
