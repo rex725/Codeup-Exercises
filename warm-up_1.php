20a846b956890b7a8dea16dae81bf32941807030
David R. Jex <david.jex@me.com>
2017-07-21 10:11:30 -0500

    Refactor to lessen length of code by adding the negative number change to evalutations of whether the number is odd or even.

--- warm-up_1.php
+++ warm-up_1.php
@@ -2,22 +2,15 @@
 $numbers = array(2,1,-5,"bob");
 $arrayEquation = 0;
 foreach($numbers as $number) {
-	
 	if (is_numeric($number) === false) {
 		$arrayEquation += 0;
 		echo "Not a number.".PHP_EOL;
-	} else if ($number < 0){
-		$number = abs($number);
-		if ($number % 2 != 0) {
-		$arrayEquation += $number;
-		} else if ($number % 2 === 0) {
-		$arrayEquation *= $number;
-		}
-		echo "Number being evaluated to positive.".PHP_EOL;
 	} else if ($number % 2 != 0) {
+		$number = abs($number);
 		$arrayEquation += $number;
 		echo "Number is odd and is being added.".PHP_EOL;
 	} else if ($number % 2 === 0) {
+		$number = abs($number);
 		$arrayEquation *= $number;
 		echo "Number is even and being multiplied.".PHP_EOL;
 	}
