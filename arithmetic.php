a7e79d5b06fced652dda39e2375a7ebe8cc78ea2
David R. Jex <david.jex@me.com>
2017-07-22 15:32:50 -0500

    Added code to echo functions and return values.

--- arithmetic.php
+++ arithmetic.php
@@ -4,20 +4,24 @@ function add($a, $b)
 {
     return $a + $b;
 }
+echo add(5, 7).PHP_EOL;
 
 function subtract($a, $b)
 {
     return $a - $b;
 }
+echo subtract(3, 6).PHP_EOL;
 
 function multiply($a, $b)
 {
     return $a * $b;
 }
+echo multiply(7,6).PHP_EOL;
 
 function divide($a, $b)
 {
     return $a / $b;
 }
+echo divide(100, 5).PHP_EOL;
 
 // Add code to test your functions here
